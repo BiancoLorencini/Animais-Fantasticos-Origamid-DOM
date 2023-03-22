b42ed053f783d49b8680279bff16dc37cc60b366
Lorencini <arthnilo@gmail.com>
2023-03-22 09:06:16 -0300

    animação-títulos-js

--- main.js
+++ main.js
@@ -63,4 +63,24 @@ function initScrollSmooth() {
   });
 }
 
-initScrollSmooth()
+initScrollSmooth();
+
+function initAnimationScroll() {
+  const sections = document.querySelectorAll(".js-scroll");
+  if (sections.length) {
+    const halfWindow = window.innerHeight * 0.6;
+
+    function animaScroll() {
+      sections.forEach((section) => {
+        const sectionTop = section.getBoundingClientRect().top - halfWindow;
+        if (sectionTop < 0) {
+          section.classList.add("ativo");
+        }
+      });
+    }
+    animaScroll();
+    window.addEventListener("scroll", animaScroll);
+  }
+}
+
+initAnimationScroll()

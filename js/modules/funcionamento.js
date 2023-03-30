9409bd20fa39809715fe2593b83c315e377f4e3d
Lorencini <arthnilo@gmail.com>
2023-03-30 09:52:20 -0300

    Indicador-horario-completo

--- js/modules/funcionamento.js
+++ js/modules/funcionamento.js
@@ -1,6 +1,19 @@
 export default function initFuncionamento() {
-  
-}
+  // pegando um elemento com data e transformando em uma array com string e convertendo para Number
+  const funcionamento = document.querySelector("[data-semana]");
+  const diasSemana = funcionamento.dataset.semana.split(",").map(Number);
+  const horarioSemana = funcionamento.dataset.horario.split(",").map(Number);
+  //----------------------------------------------
 
-const funcionamento = document.querySelector("[data-semana]");
-const diasSemana = funcionamento.dataset.semana;
+  const dataAgora = new Date();
+  const diaAgora = dataAgora.getDay();
+  const horarioAgora = dataAgora.getHours();
+
+  const semanaAberto = diasSemana.indexOf(diaAgora) !== -1;
+  const horarioAberto =
+    horarioAgora >= horarioSemana[0] && horarioAgora < horarioSemana[1];
+
+  if (semanaAberto && horarioAberto) {
+    funcionamento.classList.add("aberto");
+  }
+}
